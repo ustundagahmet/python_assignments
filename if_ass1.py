2ff7f5d72a3afb97681c737f010464f776281bd9
ahmet <neustundagahmet@gmail.com>
2020-07-07 03:37:08 -0700

    Assignment-1 (if-Statements)

---
+++ if_ass1.py
@@ -0,0 +1,7 @@
+name = "Joseph"
+e_name = input("Enter the first name:")
+
+if name == e_name :
+    print(f"Hello,{e_name}! The password is W@12")
+else:
+    print(f"Hello, {e_name}! See you later.")
